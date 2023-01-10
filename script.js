075b84ed9d7bd6f139a3ea18c1fd654139d14582
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-10 15:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ let textValue = document.getElementById("evaluatedText");
 let count = document.getElementById("letterCount");
 
 textValue.addEventListener("input", function() {
-	if(this.value == ""){
-		count.innerText = 0;
-	}
+	
 	count.innerText = this.value.length;
 	
 })
